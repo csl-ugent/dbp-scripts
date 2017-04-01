b7133c2daa2b4663ce5fb361c3132d76a3d73f06
babrath <bert.abrath@UGent.be>
2017-04-01 12:37:20 +0200

    Improved NOP setup: combined SPEC tree copying and actual Diablo runs, so I/O and CPU are used at the same moment.

--- setup_nop.py
+++ setup_nop.py
@@ -27,18 +27,16 @@ def run_diablo(name, nop_seed, input_dir, output_dir):
     except Exception:
         logging.getLogger().exception('NOP insertion failed for ' + input_dir + '/' + str(nop_seed))
 
-# Clean up previous run and create directories
-print('************ Preparing for NOP insertion by copying binaries **********')
-for (sp_seed, fs_seed, nop_seed) in support.seeds_gen(seed.SPSeed, seed.FSSeed, seed.NOPSeed):
-    for subset in support.subsets_gen((sp_seed, fs_seed)):
-        support.copy_spec_tree(support.create_path_for_seeds(config.build_dir, *subset), support.create_path_for_seeds(config.build_dir, nop_seed, *subset))
-
 # Submitting Diablo runs as tasks to the executor
 print('************ Inserting NOPs using Diablo **********')
 with concurrent.futures.ProcessPoolExecutor() as executor:
     for (sp_seed, fs_seed, nop_seed) in support.seeds_gen(seed.SPSeed, seed.FSSeed, seed.NOPSeed):
-        for (benchmark, name) in support.benchmarks_gen():
-            for subset in support.subsets_gen((sp_seed, fs_seed)):
+        for subset in support.subsets_gen((sp_seed, fs_seed)):
+            # Clean up previous run and create directories
+            support.copy_spec_tree(support.create_path_for_seeds(config.build_dir, *subset), support.create_path_for_seeds(config.build_dir, nop_seed, *subset))
+
+            # Submit the Diablo runs
+            for (benchmark, name) in support.benchmarks_gen():
                 input_dir = os.path.join(support.create_path_for_seeds(config.build_dir, *subset), benchmark)
                 output_dir = os.path.join(support.create_path_for_seeds(config.build_dir, nop_seed, *subset), benchmark)
                 executor.submit(run_diablo, name, nop_seed, input_dir, output_dir)
