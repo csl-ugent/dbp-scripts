ee7abe250c37440a2545833c62af3e03b96c67fa
babrath <bert.abrath@UGent.be>
2018-02-09 11:01:06 +0100

    Use the right objcopy (for right architecture) to do the delta data injection.

--- delta_data.py
+++ delta_data.py
@@ -61,7 +61,7 @@ def decode(dd_enc, seed_types, encrypt=True):
 # Injects the delta data into a separate section in the binary.
 # The delta data is in a file for easier objcopy use.
 def inject(binary, dd_path):
-    subprocess.check_call(['objcopy', '--input-target', 'elf32-little', '--add-section', section_name + '=' + dd_path, binary])
+    subprocess.check_call([config.target_triple + '-objcopy', '--add-section', section_name + '=' + dd_path, binary])
 
 # Extracts the delta data from the separate section in the binary.
 # The data returned is binary.
